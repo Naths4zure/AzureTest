6980aa72745722154db4c07fb3a8c14bd007c3de
Nath <Naths4zure@hotmail.com>
2023-03-30 14:13:26 +1300

    Scaffold the model ‘Movie’
    Scaffold the model ‘Movie’

---
+++ Pages/Movies/Index.cshtml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using AzureTest.Data;
+using AzureTest.Models;
+
+namespace AzureTest.Pages.Movies
+{
+    public class IndexModel : PageModel
+    {
+        private readonly AzureTest.Data.AzureTestContext _context;
+
+        public IndexModel(AzureTest.Data.AzureTestContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Movie> Movie { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            if (_context.Movie != null)
+            {
+                Movie = await _context.Movie.ToListAsync();
+            }
+        }
+    }
+}
