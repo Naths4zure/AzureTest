7031d70672580e4360de367f5c8d8d1115c230bc
Nath <Naths4zure@hotmail.com>
2023-03-31 17:18:52 +1300

    Migrate to Azure SQL database

---
+++ Migrations/20230331040724_Migrate_to_Azure_SQL_database.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AzureTest.Migrations
+{
+    /// <inheritdoc />
+    public partial class Migrate_to_Azure_SQL_database : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
