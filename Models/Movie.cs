c7f196df2ef6e9f1cbdc9d2f71ec2325b617d620
Nath <Naths4zure@hotmail.com>
2023-04-06 10:08:45 +1200

    Annotate the object 'Movies.cs'
    In the object 'Movies'
    - change the column display from 'ReleaseDate to 'Release Date'.
    - map Price from 'decimal' to 'currency' in the SQL database.

--- Models/Movie.cs
+++ Models/Movie.cs
@@ -8,12 +8,12 @@ namespace AzureTest.Models
         public int Id { get; set; }
         public string? Title { get; set; }
         
-        // [Display(Name ="Release Date")]                 // Set the column name
+        [Display(Name ="Release Date")]                 // Set the column name
         [DataType(DataType.Date)]                       // Set the SQL data type to Date
         public DateTime ReleaseDate { get; set; }
         public string? Genre { get; set; }
 
-        // [Column(TypeName = "decimal(18, 2)")]           // Set the SQL data type to currency
+        [Column(TypeName = "decimal(18, 2)")]           // Set the SQL data type to currency
         public decimal Price { get; set; }  
     }
 }
