cd22a5f25360ee5493b0eb9b8cb46942d4ee7562
Nath <Naths4zure@hotmail.com>
2023-04-16 13:17:21 +1200

    Update GitHub

--- Pages/Movies/Edit.cshtml.cs
+++ Pages/Movies/Edit.cshtml.cs
@@ -2,11 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using AzureTest.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
-using AzureTest.Data;
 using AzureTest.Models;
 
 namespace AzureTest.Pages.Movies
